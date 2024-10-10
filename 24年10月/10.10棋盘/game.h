168b403085d194e605bad2a5344c64854f352b63
coolee-hnu <972038526@qq.com>
2024-10-10 16:59:08 +0800

    棋盘（下棋游戏）
    打印棋盘

---
+++ 24年10月/10.10棋盘/game.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+
+#define ROW 10
+#define COL 10
+
+
+//≥ı ºªØ∆Â≈Ã
+void InitBoard(char board[ROW][COL],int row,int col);
+
+//¥Ú”°∆Â≈Ã
+void DisplayBoard(char board[ROW][COL], int row, int col);
