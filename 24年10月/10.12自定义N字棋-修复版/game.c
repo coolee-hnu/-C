4c9edc9f61c3f4dbac1b4f9c5c579a065fd1adaf
coolee-hnu <972038526@qq.com>
2024-10-12 09:36:57 +0800

    N字棋-调试完成
    成功实现对角线元素检测

---
+++ 24年10月/10.12自定义N字棋-修复版/game.c
@@ -0,0 +1,213 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "game.h"
+
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			//打印数据
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+			{
+				printf("|");
+			}
+			
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			int j = 0;
+			for (j = 0; j < col; j++)
+			{
+				//打印分割行
+				printf("---");
+				if (j < col - 1)
+				{
+					printf("|");
+				}
+
+			}
+		}
+			printf("\n");
+	}
+}
+
+//玩家下棋
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	printf("玩家请下棋\n");
+	int x = 0;
+	int y = 0;
+
+	while (1)
+	{
+		printf("请输入坐标：");
+		scanf("%d %d", &x, &y);
+		if (x>0 && x - 1 < row && y>0 && y - 1 < col)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("此处已落子，请重新输入\n");
+			}
+		}
+		else
+		{
+			printf("坐标非法，请重新输入\n");
+		}
+	}
+}
+//电脑下棋
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	printf("电脑下棋\n");
+	int x = rand() % row;
+	int y = rand() % col;
+	while (1)
+	{
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+
+	}
+}
+
+//平局判断函数
+int IsFull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;
+				break;
+			}
+		}
+	}
+	return 1;
+}
+
+char IsWin(char board[ROW][COL], int row, int col) 
+{
+	//判断玩家或者电脑赢
+	int i = 0;
+	int count1 = 1;
+	int count2 = 1;
+	//行、列
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			//行
+			if (board[i][j] != ' ' && board[i][j + 1] == board[i][j])
+			{
+				count1++;
+				if (count1 == play)
+				{
+					return board[i][j];
+					break;
+				}
+			}
+			if (board[j][i] != ' ' && board[j + 1][i] == board[j][i])
+			{
+				//列
+				count2++;
+				if (count2 == play)
+				{
+					return board[j][i];
+					break;
+				}
+			}
+		}
+		count1 = 1;
+		count2 = 1;
+		
+	}
+	//沿正对角线
+	for (i = 0; i < row-play; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col-play; j++)
+		{
+				int n = 0;
+				for (n = 1; n < play; n++)
+				{
+					if (board[i][j] != ' ' && board[i + n][j + n] == board[i][j])
+					{
+						count1++;
+						if (count1 == play)
+						{
+							return board[i][j];
+							break;
+						}
+					}
+
+				}
+		}
+		count1 = 1;
+	}
+
+	//沿副对角线
+	for (i = 0; i < row - play; i++)
+	{
+		int j = 0;
+		for (j = col; j > col - play; j--)
+		{
+			int n = 0;
+			for (n = 1; n < play; n++)
+			{
+				if (board[i][j] != ' ' && board[i + n][j - n] == board[i][j])
+				{
+					count2++;
+					if (count2 == play)
+					{
+						return board[i][j];
+						break;
+					}
+				}
+
+			}
+		}
+		count2 = 1;
+
+	}
+
+
+	//平局
+	if (IsFull(board, row, col))
+	{
+		return 'Q';
+	}
+
+	//继续
+	return 'C';
+
+}
